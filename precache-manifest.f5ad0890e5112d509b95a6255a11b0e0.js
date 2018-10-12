e53e64489d2b9c1855f19963c03ea0889e192005
imfanjs <imfanjs@gmail.com>
2018-10-12 12:41:00 -0600

    Updates

---
+++ precache-manifest.f5ad0890e5112d509b95a6255a11b0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb3e525d69c23a0d546",
+    "url": "/walmart-dashboard/static/js/runtime~main.5fb3e525.js"
+  },
+  {
+    "revision": "ebc6f60db8a8e48b33ce",
+    "url": "/walmart-dashboard/static/js/main.ebc6f60d.chunk.js"
+  },
+  {
+    "revision": "0b94e9263edbce04c0dd",
+    "url": "/walmart-dashboard/static/js/1.0b94e926.chunk.js"
+  },
+  {
+    "revision": "ebc6f60db8a8e48b33ce",
+    "url": "/walmart-dashboard/static/css/main.4ffb330d.chunk.css"
+  },
+  {
+    "revision": "0b94e9263edbce04c0dd",
+    "url": "/walmart-dashboard/static/css/1.86fbe412.chunk.css"
+  },
+  {
+    "revision": "047826d812eaf159e2bd2f519ca63cd1",
+    "url": "/walmart-dashboard/index.html"
+  }
+];
